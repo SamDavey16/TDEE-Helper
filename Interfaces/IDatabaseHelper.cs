a39c4b5db526324468f5eb958cd6e1dcdcf126f2
samdavey16 <sam.davey@talktalk.net>
2025-06-01 16:05:38 +0100

    Refactored databasehelper to be a service that can utilise dependency injection

---
+++ Interfaces/IDatabaseHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeightTracker.Models;
+
+namespace WeightTracker.Interfaces
+{
+    public interface IDatabaseHelper
+    {
+        Task GetCurrentTDEE(int userId);
+        Task AddEntry(Entries entry);
+        Task<int> CreateNewUser(Users user);
+    }
+}
